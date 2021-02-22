07123f4392024d850b465c9dd12038de0f747c94
nicholasmirchandani <nmirchandani@chapman.edu>
2021-02-21 21:45:58 -0800

    Adds Primitive Networking :)

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Net;
+using System;
+using System.Text;
+
+
+public class NetworkManager : MonoBehaviour {
+    public static NetworkManager Instance;
+    public ulong seed;
+    public bool connected;
+    public bool isServer;
+
+    private Socket handler;
+    private Socket sender;
+
+    public void Start() {
+        if(Instance == null) {
+            Instance = this;
+        } else {
+            Destroy(this);
+        }
+
+        connected = false;
+        isServer = false;
+    }
+
+    public void StartServer() {
+        IPHostEntry host = Dns.GetHostEntry("127.0.0.1");
+        IPAddress ipAddress = host.AddressList[0];
+        Debug.Log("IP Address: " + ipAddress.ToString());
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11234); //Running on port 11234
+
+        try {
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint);
+
+            listener.Listen(1);
+
+            Debug.Log("Waiting for a connection...");
+            //TODO: Close the socket when clicking off of Host screen
+            //listener.BeginAccept(new AsyncCallback(OnSocketAccept), listener);
+            handler = listener.Accept();
+            Debug.Log("Connection made!");
+            byte[] dataBytes = new byte[100];
+            string data = null;
+            int bytesRec = handler.Receive(dataBytes);
+            data += Encoding.ASCII.GetString(dataBytes, 0, bytesRec);
+            Debug.Log("Read: " + data);
+
+            seed = ulong.Parse(data.Substring(6));
+            //Shut down socket and close connection
+            //handler.Shutdown(SocketShutdown.Both);
+            //handler.Close();
+            Debug.Log("Seed is: " + seed);
+            connected = true;
+            isServer = true;
+        } catch (Exception e) {
+            Debug.Log("ERROR: " + e.ToString());
+        }
+    }
+
+    public void JoinServer(string ip) {
+        //replace 127.0.0.1 with an actual ip
+        IPHostEntry host = Dns.GetHostEntry(ip);
+        IPAddress ipAddress = host.AddressList[0];
+        Debug.Log("IP Address: " + ipAddress.ToString());
+        IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11234); //Running on port 11234
+
+        sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        try {
+            sender.Connect(remoteEP);
+
+            Debug.Log("Connected!");
+            //TODO: fix this jank ass code
+            seed = (ulong) (UnityEngine.Random.Range(0, Int32.MaxValue)) * (Int32.MaxValue) + (ulong) UnityEngine.Random.Range(0, Int32.MaxValue);
+            byte[] msg = Encoding.ASCII.GetBytes("Seed: " + seed.ToString());
+
+            int bytesSent = sender.Send(msg);
+            connected = true;
+            isServer = false;
+        } catch (Exception e) {
+            Debug.Log("ERROR: " + e.ToString());
+        }
+    }
+
+    public void SendString(string message) {
+        if(connected) {
+            byte[] dataBytes = new byte[100];
+            if (isServer) {
+                handler.Send(Encoding.ASCII.GetBytes(message));
+            } else {
+                sender.Send(Encoding.ASCII.GetBytes(message));
+            }
+        } else {
+            Debug.Log("ERROR: Trying to send message without being connected");
+        }
+    }
+
+    public string ReceiveString() {
+        if (connected) {
+            byte[] dataBytes = new byte[100];
+            string message;
+            if (isServer) {
+                int bytesRec = handler.Receive(dataBytes);
+                message = Encoding.ASCII.GetString(dataBytes, 0, bytesRec);
+            } else {
+                int bytesRec = sender.Receive(dataBytes);
+                message = Encoding.ASCII.GetString(dataBytes, 0, bytesRec);
+            }
+            return message;
+        } else {
+            Debug.Log("ERROR: Trying to send message without being connected");
+            return "ERROR";
+        }
+    }
+
+    public void CloseSockets() {
+        if(connected) {
+            if(isServer) {
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            } else {
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+        }
+        connected = false;
+    }
+}
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,12 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class Menu : MonoBehaviour
-{
-    public void LoadScene(int scene)
-    {
+public class Menu : MonoBehaviour {
+    public GameObject mainMenu;
+    public GameObject multiplayerMenu;
+    public GameObject hostMenu;
+    public GameObject joinMenu;
+    public Text HostIPText;
+    public InputField JoinIP;
+
+    public void Start() {
+        mainMenu.SetActive(true);
+        multiplayerMenu.SetActive(false);
+        joinMenu.SetActive(false);
+        hostMenu.SetActive(false);
+    }
+
+    public void LoadScene(int scene) {
         SceneManager.LoadScene(scene);
+    }
+
+    public void ShowMultiplayer() {
+        mainMenu.SetActive(false);
+        multiplayerMenu.SetActive(true);
+        joinMenu.SetActive(false);
+        hostMenu.SetActive(false);
+    }
+
+    public void HideMultiplayer() {
+        mainMenu.SetActive(true);
+        multiplayerMenu.SetActive(false);
+        joinMenu.SetActive(false);
+        hostMenu.SetActive(false);
+    }
+
+    public void HostGame() {
+        mainMenu.SetActive(false);
+        multiplayerMenu.SetActive(false);
+        hostMenu.SetActive(true);
+        joinMenu.SetActive(false);
+        HostIPText.text = "Your IP is " + Dns.GetHostEntry("127.0.0.1").AddressList[0].ToString();
+        NetworkManager.Instance.StartServer();
+        NetworkManager.Instance.SendString("TEST MESSAGE 2");
+    }
+
+    public void JoinGameMenu() {
+        mainMenu.SetActive(false);
+        multiplayerMenu.SetActive(false);
+        joinMenu.SetActive(true);
+        hostMenu.SetActive(false);
+    }
+
+    public void JoinGame() {
+        Debug.Log("Joining: "+ JoinIP.text);
+        NetworkManager.Instance.JoinServer(JoinIP.text);
+        string testString = NetworkManager.Instance.ReceiveString();
+        Debug.Log(testString);
     }
 }
